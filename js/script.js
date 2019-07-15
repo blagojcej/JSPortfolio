a41367ad38c2654f9b55f840db47c5d8dcae107a
blagojcej <blagojcejovanoski@yahoo.com>
2019-07-15 22:24:44 +0200

    Finished all sections: Slides, Navigation, About, Skills Stats, Contact and Copyright section.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+$(window).on("load", function () {
+    //fadeout inner element first
+   $(".loader .inner").fadeOut(500, function () {
+       //fadeout background
+       $(".loader").fadeOut(750);
+   });
+
+   $(".items").isotope({
+    filter: '*',
+    animationOptions: {
+        duration:1500,
+        easing: 'linear',
+        queue: false
+    }
+});
+
+});
+
 $(document).ready(function () {
     $('#slides').superslides({
         animation: 'fade',
@@ -66,4 +84,61 @@ $(document).ready(function () {
     //     } else {
     //     console.error(countUp.error);
     //     }
+
+    $("[data-fancybox]").fancybox();
+    
+    $("#filters a").click(function() {
+        //get element with "current" class and remove the class
+        $("#filters .current").removeClass("current");
+
+        //add "current" class to current clicked element
+        $(this).addClass("current");
+
+        //get "data-filter" value from attribute from current a tag
+        var selector=$(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration:1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        //Don't do anything else, stop proceeding click event
+        return false;
+    });
+
+    $("#navigation li a").click(function (e) {
+       //Prevent default behavior on click
+       e.preventDefault();
+       
+       //Get the "href" attribute on clicked element
+       var targetElement=$(this).attr("href");
+
+       //Get the top of target element - where click event will lead us
+       var targetPosition=$(targetElement).offset().top;
+
+       $("html, body").animate({scrollTop: targetPosition - 50}, "slow");
+    });
+
+    const nav= $("#navigation");
+    const navTop=nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+        const body=$("body");
+
+        if($(window).scrollTop() >= navTop) {
+            body.addClass('fixedNav');
+            body.css('padding-top', nav.outerHeight()+'px');
+        }
+        else {
+            body.css('padding-top', 0);
+            body.removeClass('fixedNav');
+        }
+    }
 });
